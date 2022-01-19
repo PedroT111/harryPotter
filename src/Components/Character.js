eb2dc500f884767e82ee20999ca4bdf94ada4017
pedro <pedrochincolla11@gmail.com>
2022-01-19 14:29:19 -0300

    character component

--- src/Components/Character.js
+++ src/Components/Character.js
@@ -6,11 +6,11 @@ const Character = (props) => {
     let {character} = props;
     return(
         <>
-        <div className="card mb-4">
+        <div className="card" key={character.id}>
             <div>
-                <img src={character.image} className="card-img-top" style={{widt:'100%', height:'300px'}} alt='..'/>
-                <div>
-                    <h3 className="fs-6 mt-3 px-3 d-inline-block">{character.name}</h3>
+                <img src={character.imagen} className="card-img-top" style={{widt:'100%', height:'300px'}} alt='..'/>
+                <div className="name">
+                    <h3 className="fs-6 text-center mt-2">{character.personaje}</h3>
                 </div>
             </div>
         </div>
