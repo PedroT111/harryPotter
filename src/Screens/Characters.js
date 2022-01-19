5c165aa7fd9d30ab4c687bc0f50c7f699d643694
pedro <pedrochincolla11@gmail.com>
2022-01-19 14:27:22 -0300

    characters screen

--- src/Screens/Characters.js
+++ src/Screens/Characters.js
@@ -3,46 +3,50 @@ import axios from 'axios';
 import Character from "../Components/Character";
 import '../Styles/characters.css'
 import Pagina from '../Components/Pagination';
-
-
-const URL_CHARACTERS = 'http://hp-api.herokuapp.com/api/characters/';
+import Spinner from "../Components/Spinner";
+import { useDispatch, useSelector } from "react-redux";
+import { getCharactersAction } from "../store/actions/Characters";
 
 const Characters =  () => {
-    const[characters, setCharacters] = useState(null);
     const[currentPage, setCurrentPage] = useState(1);
-    const[charactersPerPage, setCharactersPerPage] = useState(8);
-
-    const getData = async () => {
-            let {data: personajes} = await axios(URL_CHARACTERS);
-            setCharacters(personajes);
-    }
-    
-    useEffect(() => {
-        getData();
-    }, []);
-
+    const[charactersPerPage] = useState(8);
     const indexOfLastCharacter = currentPage * charactersPerPage;
     const indexOfFirstCharacter = indexOfLastCharacter - charactersPerPage;
-    const currentCharacter = characters.slice(indexOfFirstCharacter, indexOfLastCharacter);
-
     const paginate = pageNumber => setCurrentPage(pageNumber);
-    
+    /*----------------------------------------------------*/
+    const loading = useSelector(state => state.characters.loading)
+    const Characters = useSelector(state => state.characters.characters)
+   const CurrentCharacters = Characters.slice(indexOfFirstCharacter, indexOfLastCharacter);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const getCharacters = () => dispatch(getCharactersAction());
+        getCharacters();
+    },[dispatch]);
+   /*------------------------------------------------------*/
+
     return(
         <>
-        <h1>Personajes</h1>
+        {loading ? (<Spinner/>): null}
         <div className="conteiner py-4 position-relative">
+        <div className="characters">
         {
-            currentCharacter.map(character =>
-                <Character
-                character = {character}
-                key={character.name}/>
+            Characters === 0 ? console.log("No hay personajes") : (
+                CurrentCharacters.map(character => (
+                    <div className="grid-item">
+                         <Character
+                        character ={character}
+                        key={character.id}/>
+                    </div>
+                )                   
+            )
             )
         }
+        </div> 
         <Pagina
-        totalCharacters = {40}
-        charactersPerPage={charactersPerPage}
         paginate={paginate}
-        />
+        totalCharacters={23}
+        charactersPerPage={charactersPerPage}
+        />       
         </div>
         </>
     )
