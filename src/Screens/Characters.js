b8c5837c277c72ffd15ffd459b483d2f28c8fef6
pedro <pedrochincolla11@gmail.com>
2022-01-19 16:23:52 -0300

    action filter

--- src/Screens/Characters.js
+++ src/Screens/Characters.js
@@ -6,6 +6,7 @@ import Pagina from '../Components/Pagination';
 import Spinner from "../Components/Spinner";
 import { useDispatch, useSelector } from "react-redux";
 import { getCharactersAction } from "../store/actions/Characters";
+import Filter from "../Components/Filter";
 
 const Characters =  () => {
     const[currentPage, setCurrentPage] = useState(1);
@@ -13,41 +14,43 @@ const Characters =  () => {
     const indexOfLastCharacter = currentPage * charactersPerPage;
     const indexOfFirstCharacter = indexOfLastCharacter - charactersPerPage;
     const paginate = pageNumber => setCurrentPage(pageNumber);
-    /*----------------------------------------------------*/
+
     const loading = useSelector(state => state.characters.loading)
-    const Characters = useSelector(state => state.characters.characters)
+    const Characters = useSelector(state => state.characters.filterCharacters)
    const CurrentCharacters = Characters.slice(indexOfFirstCharacter, indexOfLastCharacter);
     const dispatch = useDispatch();
     useEffect(() => {
         const getCharacters = () => dispatch(getCharactersAction());
         getCharacters();
     },[dispatch]);
-   /*------------------------------------------------------*/
 
     return(
         <>
         {loading ? (<Spinner/>): null}
-        <div className="conteiner py-4 position-relative">
-        <div className="characters">
-        {
-            Characters === 0 ? console.log("No hay personajes") : (
-                CurrentCharacters.map(character => (
-                    <div className="grid-item">
-                         <Character
-                        character ={character}
-                        key={character.id}/>
-                    </div>
-                )                   
-            )
-            )
-        }
-        </div> 
-        <Pagina
-        paginate={paginate}
-        totalCharacters={23}
-        charactersPerPage={charactersPerPage}
-        />       
-        </div>
+        <Filter/>
+            <div className="conteiner py-2 position-relative">
+            <div className="characters">
+            {
+                Characters === 0 ? console.log("No hay personajes") : (
+                    CurrentCharacters.map(character => (
+                        <div className="grid-item">
+                            <Character
+                            character ={character}
+                            key={character.id}/>
+                        </div>
+                    )                   
+                )
+                )
+            }
+            </div> 
+            <Pagina
+            paginate={paginate}
+            totalCharacters={Characters.length}
+            charactersPerPage={charactersPerPage}
+            />       
+            </div>
+
+        
         </>
     )
 }
