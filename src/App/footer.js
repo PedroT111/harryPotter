819d43823d51be366f15328346c62119b5a00f67
pedro <pedrochincolla11@gmail.com>
2022-01-24 18:14:08 -0300

    add links footer

--- src/App/footer.js
+++ src/App/footer.js
@@ -1,21 +1,30 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithubSquare} from 'react-icons/fa'
+import '../Styles/footer.css'
 
 const Footer = () => {
     return(
-        <Container flui className="">
+        <Container fluid>
             <Row className="justify-content-center text-light">
                 <Col md={10} className="d-flex justify-content-between p-4">
-                <div>
-                    <a href="" class="text-white">
-                    <i class="fab fa-facebook-f">dwq</i>
-                    </a>
-                    <a href="" class="text-white">
-                    <i class="fab fa-twitter">dwqdwq</i>
-                    </a>
+                <div className="followme">
+                    <a href="https://linkedin.com/in/pedrochincolla" 
+                       target='_blank' 
+                       rel="noreferrer"
+                       className="links">
+                        <BsLinkedin size='20'/> Linkedin 
+                        </a>
+                    <a href="https://github.com/PedroT111" 
+                       target='_blank' 
+                       rel="noreferrer"
+                       className="links">
+                        <FaGithubSquare size='25'/> GitHub 
+                        </a>                      
                 </div>
                 <div>
-                    <span>Pedro Chincolla</span>
+                    <span>Pedro Chincolla © 2022</span>
                 </div>
                 </Col>
             </Row>
