17c2466a7b93b33a94e74e58a99b562278e0aa40
pedro <pedrochincolla11@gmail.com>
2022-01-24 17:24:04 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React from "react";
 import Characters from "./Screens/Characters";
+import Features from "./Screens/Features";
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Route, Switch,
+} from 'react-router-dom';
+import Header from "./App/header";
+import Footer from "./App/footer";
 
 function App() {
   return (
     <div className="App">
-    <Characters/>
+      <Router>
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={Characters}/>
+          <Route exact path="/:id" component={Features}/>
+        </Switch>
+        <Footer/>
+      </Router>
     </div>
   );
 }
